e46a580f317e08b8b4a7b498d54c867ee6a0cc57
yayakaulia <juhabch@gmail.com>
2022-07-20 17:05:26 +0700

    predict fastapi ready

--- app/src/api.py
+++ app/src/api.py
@@ -1,11 +1,52 @@
 from predict import main_predict
-from fastapi import FastAPI, Form
+from fastapi import FastAPI
+from utils import read_yaml
+import joblib
+from pydantic import BaseModel
+
+
+PREPROCESSING_CONFIG_PATH = "../config/preprocessing_config.yaml"
+PREDICT_CONFIG_PATH = "../config/predict_config.yaml"
+
+params_prep = read_yaml(PREPROCESSING_CONFIG_PATH)
+model = joblib.load('../output/model_name.pkl')
 
 app = FastAPI()
 
-@app.post("/predict")
-def predict_api(data_predict = Form()):
-    result = main_predict(data_predict)
+class Item(BaseModel):
+    ID: int
+    LIMIT_BAL: int
+    SEX: int
+    EDUCATION: int
+    MARRIAGE: int
+    AGE: int
+    PAY_0: int
+    PAY_2: int
+    PAY_3: int
+    PAY_4: int
+    PAY_5: int
+    PAY_6: int
+    BILL_AMT1: int
+    BILL_AMT2: int
+    BILL_AMT3: int
+    BILL_AMT4: int
+    BILL_AMT5: int
+    BILL_AMT6: int
+    PAY_AMT1: int
+    PAY_AMT2: int
+    PAY_AMT3: int
+    PAY_AMT4: int
+    PAY_AMT5: int
+    PAY_AMT6: int
+
+@app.post("/predict/")
+def predict_api(item: Item):
+    data_predict = {}
+
+    for i, value in enumerate(item):
+        data_predict[value[0]] = [value[1]]
+    result = main_predict(data_predict, model=model, params_prep=params_prep)
+
     return {
-        'result' : result
+        "result": result
     }
